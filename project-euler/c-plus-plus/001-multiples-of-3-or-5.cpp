a626a733a14b6f061e19b84a08ab9b2407462372
Noey Ignacio <hello@noeyislearning.dev>
2023-10-02 16:19:36 +0800

    build(`project-euler`): 001 `cpp`

---
+++ project-euler/c-plus-plus/001-multiples-of-3-or-5.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main() {
+    
+    int sum = 0;
+
+    for (int i = 1; i < 1000; i++) {
+        if (i % 3 == 0 || i % 5 == 0) {
+            sum += i;
+        }
+    }
+
+    std::cout << "The sum of all the multiples of 3 or 5 below 1000 is: " << sum << std::endl;
+
+    return 0;
+}
