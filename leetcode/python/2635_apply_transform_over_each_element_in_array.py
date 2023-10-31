ca1481c8654d92c2d77af644589e7cc27d63d311
Noey Ignacio <hello@noeyislearning.dev>
2023-10-31 21:41:41 +0800

    refactor/leetcode: 2635, translate from JS to PY

--- leetcode/python/2635_apply_transform_over_each_element_in_array.py
+++ leetcode/python/2635_apply_transform_over_each_element_in_array.py
@@ -1,3 +1,9 @@
+"""
+2635. Apply Transform over Each Element in Array (Answer v1)
+Problem Link: https://leetcode.com/problems/apply-transform-over-each-element-in-array/
+Solution Link (Personal):
+"""
+
 from typing import List, Callable
 
 def map(arr: List, fn: Callable) -> List:
