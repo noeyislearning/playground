258cb6ff39d7d9bafe3947942e405be61c425330
Noey Ignacio <hello@noeyislearning.dev>
2023-10-31 17:52:34 +0800

    build/leetcode: solve 176 second highest salary

---
+++ leetcode/pandas/0176_second_highest_salary_v1.py
@@ -0,0 +1,22 @@
+"""
+0176. Second Highest Salary (Ansnwer: v1)
+Problem Link: https://leetcode.com/problems/second-highest-salary/
+Solution Link + Explanation: https://leetcode.com/problems/second-highest-salary/solutions/4230149/finding-the-second-highest-salary/
+"""
+import pandas as pd
+
+def second_highest_salary(employee: pd.DataFrame) -> pd.DataFrame:
+    # Sort the DataFrame by the 'salary' column in descending order
+    sorted_employee = employee.sort_values(by='salary', ascending=False)
+    
+    # Drop any duplicate salary values (in case there are multiple employees with the same salary)
+    sorted_employee = sorted_employee.drop_duplicates(subset='salary')
+    
+    # Check if there are at least two distinct salary values
+    if sorted_employee.shape[0] < 2:
+        return pd.DataFrame({'SecondHighestSalary': [None]})
+    
+    # Get the second row (which has the second-highest salary)
+    second_highest = sorted_employee.iloc[1]
+    
+    return pd.DataFrame({'SecondHighestSalary': [second_highest['salary']]})
