9807c352c0388ede186b8b4540787caef7ddbbd3
Noey Ignacio <hello@noeyislearning.dev>
2023-11-03 01:41:14 +0800

    build/leetcode: 177 PostgreSQL challenge

---
+++ leetcode/sql/postgresql/0177_nth_highest_salary_v1.sql
@@ -0,0 +1,16 @@
+-- 177. Nth Highest Salary
+-- Problem Link: https://leetcode.com/problems/nth-highest-salary/
+-- Solution Link (Personal):
+
+CREATE OR REPLACE FUNCTION NthHighestSalary(N INT) RETURNS INT AS $$
+DECLARE
+  salary_value INT;
+BEGIN
+  SELECT DISTINCT Salary INTO salary_value
+  FROM Employee
+  ORDER BY Salary DESC
+  LIMIT 1 OFFSET N - 1;
+
+  RETURN salary_value;
+END;
+$$ LANGUAGE plpgsql;
