7c5148e95efaf80ab53a3f987eaf331ff788c995
Noey Ignacio <hello@noeyislearning.dev>
2023-11-03 10:23:37 +0800

    build/leetcode: 2666 JS challenge, daily

---
+++ leetcode/javascript/2666_allow_one_function_call_v1.js
@@ -0,0 +1,20 @@
+/**
+ * 2666. Allow One Function Call
+ * Problem Link: https://leetcode.com/problems/allow-one-function-call/
+ * Solution Link (Personal): 
+ */
+
+var once = function(fn) {
+  let called = false;
+  let result;
+
+  return function(...args) {
+      if (!called) {
+          called = true;
+          result = fn(...args);
+          return result;
+      } else {
+          return undefined;
+      }
+  };
+};
