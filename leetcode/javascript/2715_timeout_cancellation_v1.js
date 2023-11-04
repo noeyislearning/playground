a7a2d2a375ce0ba5b3ea024ff30c393625ba8b2b
Noey Ignacio <hello@noeyislearning.dev>
2023-11-04 13:19:03 +0800

    refactor/solution: build solution

--- leetcode/javascript/2715_timeout_cancellation_v1.js
+++ leetcode/javascript/2715_timeout_cancellation_v1.js
@@ -3,3 +3,21 @@
  * Problem Link: https://leetcode.com/problems/timeout-cancellation/
  * Solution Link (Personal):
  */
+
+var cancellable = function(fn, args, t) {
+  let cancelled = false;
+
+  const delayCallback = () => {
+    if (!cancelled) {
+      fn(...args);
+    }
+  };
+
+  const cancelFn = () => {
+    cancelled = true;
+  };
+
+  setTimeout(delayCallback, t);
+
+  return cancelFn;
+};
