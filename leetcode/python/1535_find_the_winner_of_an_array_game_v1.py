44fc1572be5da260d8babe967c410b307c2e24c5
Noey Ignacio <hello@noeyislearning.dev>
2023-11-05 13:38:48 +0800

    build/leetcode: 1535 PY challenge

---
+++ leetcode/python/1535_find_the_winner_of_an_array_game_v1.py
@@ -0,0 +1,23 @@
+"""
+ * 1535. Find the Winner of an Array Game
+ * Problem Link: https://leetcode.com/problems/find-the-winner-of-an-array-game/
+ * Solution Link (Personal):
+"""
+
+
+class Solution:
+    def getWinner(self, arr: List[int], k: int) -> int:
+        current_winner = arr[0]
+        win_count = 0
+
+        for i in range(1, len(arr)):
+            if arr[i] > current_winner:
+                current_winner = arr[i]
+                win_count = 1
+            else:
+                win_count += 1
+
+            if win_count == k:
+                return current_winner
+
+        return max(arr)
