6ef642ba46920e8a0bc042c745ce295d0f862b70
Noey Ignacio <hello@noeyislearning.dev>
2023-11-05 13:39:16 +0800

    refactor/docs: add new information to 2265 PY

--- leetcode/python/2265_count_nodes_equal_to_average_subtree_v1.py
+++ leetcode/python/2265_count_nodes_equal_to_average_subtree_v1.py
@@ -1,7 +1,7 @@
 """
-2265. Count Nodes Equal to Average of Subtree (Answer v1)
-Problem Link: https://leetcode.com/problems/count-nodes-equal-to-average-of-subtree/
-Solution Link (Personal):
+ * 2265. Count Nodes Equal to Average of Subtree (Answer v1)
+ * Problem Link: https://leetcode.com/problems/count-nodes-equal-to-average-of-subtree/
+ * Solution Link (Personal):
 """
 
 class TreeNode:
