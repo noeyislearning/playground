f8e930d136dba8f2aab17cab3fdb18ae01ee1c79
Noey Ignacio <hello@noeyislearning.dev>
2023-11-05 13:56:54 +0800

    build/leetcode: add 2877, 2878, 2879 Pandas Challenge

---
+++ leetcode/pandas/2878_get_the_size_of_a_dataframe_v1.py
@@ -0,0 +1,11 @@
+"""
+ * 2878. Get the Size of a DataFrame
+ * Problem Link: https://leetcode.com/problems/get-the-size-of-a-dataframe/
+ * Solution Link (Personal):
+"""
+
+from typing import List
+import pandas as pd
+
+def getDataframeSize(players: pd.DataFrame) -> List[int]:
+  return list(players.shape)
