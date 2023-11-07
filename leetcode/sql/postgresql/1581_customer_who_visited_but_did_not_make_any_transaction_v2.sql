8fdd9b8605d35e588c8e181dfe444636434fd49e
Noey Ignacio <hello@noeyislearning.dev>
2023-11-07 15:38:30 +0800

    build/leetcode: 1068, 1378, 1581 v1 and v2, SQL challenge

---
+++ leetcode/sql/postgresql/1581_customer_who_visited_but_did_not_make_any_transaction_v2.sql
@@ -0,0 +1,17 @@
+-- 1581. Customer Who Visited but Did Not Make Any Transactions (Answer v2)
+-- Problem Link: https://leetcode.com/problems/customer-who-visited-but-did-not-make-any-transactions/
+-- Solution Link (Personal):
+
+
+WITH visits_without_transactions AS (
+  SELECT v.customer_id, v.visit_id
+  FROM Visits v
+  LEFT JOIN Transactions t ON v.visit_id = t.visit_id
+  WHERE t.transaction_id IS NULL
+)
+
+SELECT
+  customer_id,
+  COUNT(*) AS count_no_trans
+FROM visits_without_transactions
+GROUP BY customer_id
