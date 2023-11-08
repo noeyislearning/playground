018dea44608e934d2080ebe0fb1093e96d9332c0
Noey Ignacio <hello@noeyislearning.dev>
2023-11-08 08:40:13 +0800

    build/leetcode: 1280 SQL challenge

---
+++ leetcode/sql/postgresql/1280_students_and_examinations.sql
@@ -0,0 +1,20 @@
+-- 1280. Students and Examinations
+-- Problem Link: https://leetcode.com/problems/students-and-examinations/
+-- Solution Link (Personal):
+
+WITH Cartesian AS (
+    SELECT DISTINCT s.student_id, sub.subject_name
+    FROM Students s
+    CROSS JOIN Subjects sub
+)
+
+SELECT
+    c.student_id,
+    s.student_name,
+    c.subject_name,
+    COUNT(e.subject_name) AS attended_exams
+FROM Cartesian c
+JOIN Students s ON c.student_id = s.student_id
+LEFT JOIN Examinations e ON c.student_id = e.student_id AND c.subject_name = e.subject_name
+GROUP BY c.student_id, s.student_name, c.subject_name
+ORDER BY c.student_id, c.subject_name;
