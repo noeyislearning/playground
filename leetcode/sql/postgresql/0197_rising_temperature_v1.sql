69ef79a472a6ca3df9fe3f1b67866c32886ae2bb
Noey Ignacio <hello@noeyislearning.dev>
2023-11-08 08:17:18 +0800

    build/leetcode: 197 SQL challenge

---
+++ leetcode/sql/postgresql/0197_rising_temperature_v1.sql
@@ -0,0 +1,8 @@
+-- 197. Rising Temperature
+-- Problem Link: https://leetcode.com/problems/rising-temperature/
+-- Solution Link (Personal):
+
+SELECT w1.id
+FROM Weather w1
+JOIN Weather w2 ON w1.recordDate = w2.recordDate + 1
+WHERE w1.temperature > w2.temperature;
