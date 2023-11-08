795099821c11714df590abe13ba804e104df766f
Noey Ignacio <hello@noeyislearning.dev>
2023-11-08 09:25:05 +0800

    build/leetcode: 1934 SQL challenge

---
+++ leetcode/sql/postgresql/1934_confirmation_rate.sql
@@ -0,0 +1,14 @@
+-- 1934. Confirmation Rate
+-- Problem Link: https://leetcode.com/problems/confirmation-rate/
+-- Solution Link (Personal):
+
+SELECT
+    s.user_id,
+    CASE
+        WHEN COUNT(c.user_id) = 0 THEN 0.00
+        ELSE ROUND(SUM(CASE WHEN c.action = 'confirmed' THEN 1 ELSE 0 END) * 1.0 / COUNT(c.user_id), 2)
+    END AS confirmation_rate
+FROM Signups s
+LEFT JOIN Confirmations c ON s.user_id = c.user_id
+GROUP BY s.user_id
+
