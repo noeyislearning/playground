a19981c3a1681b1bea14beaeee321f43e90877ed
Noey Ignacio <hello@noeyislearning.dev>
2023-11-09 21:30:09 +0800

    build/leetcode: 1075 SQL challenge

---
+++ leetcode/sql/mysql/1075_project_employees_i.sql
@@ -0,0 +1,8 @@
+-- 1075. Project Employees I 
+-- Problem Link: https://leetcode.com/problems/project-employees-i/
+-- Solution Link (Personal):
+
+SELECT p.project_id, ROUND(AVG(e.experience_years), 2) AS average_years
+FROM Project p
+JOIN Employee e ON p.employee_id = e.employee_id
+GROUP BY p.project_id;
