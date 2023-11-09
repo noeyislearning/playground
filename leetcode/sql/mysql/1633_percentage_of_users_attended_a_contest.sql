d0648cc5a6e96a4b1d464fd60ee9014628bbccef
Noey Ignacio <hello@noeyislearning.dev>
2023-11-09 21:29:58 +0800

    build/leetcode: 1633 SQL challenge

---
+++ leetcode/sql/mysql/1633_percentage_of_users_attended_a_contest.sql
@@ -0,0 +1,8 @@
+-- 1633. Percentage of Users Attended a Contest
+-- Problem Link: https://leetcode.com/problems/percentage-of-users-attended-a-contest/
+-- Solution Link (Personal):
+
+SELECT r.contest_id, ROUND((COUNT(DISTINCT r.user_id) / (SELECT COUNT(*) FROM Users)) * 100, 2) AS percentage
+FROM Register r
+GROUP BY r.contest_id
+ORDER BY percentage DESC, r.contest_id ASC;
