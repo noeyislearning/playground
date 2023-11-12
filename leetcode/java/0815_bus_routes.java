3c85296db31dd43a9514e36aab9bc90c55d70cb5
Noey Ignacio <hello@noeyislearning.dev>
2023-11-12 23:39:15 +0800

    experiment/leetcode: 815 Java challenge, daily

---
+++ leetcode/java/0815_bus_routes.java
@@ -0,0 +1,42 @@
+/**
+ * 815. Bus Routes
+ * Problem Link: https://leetcode.com/problems/bus-routes/
+ * Solution Link (Personal):
+ */
+
+class Solution {
+    public int numBusesToDestination(int[][] routes, int source, int target) {
+        if (source == target) return 0;
+
+        Map<Integer, List<Integer>> stopToRoutes = new HashMap<>();
+        for (int i = 0; i < routes.length; i++) {
+            for (int stop : routes[i]) {
+                stopToRoutes.computeIfAbsent(stop, x -> new ArrayList<>()).add(i);
+            }
+        }
+
+        Queue<Integer> queue = new LinkedList<>();
+        queue.offer(source);
+        Set<Integer> visited = new HashSet<>();
+        int buses = 0;
+
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                int stop = queue.poll();
+                if (stop == target) return buses;
+
+                for (int route : stopToRoutes.get(stop)) {
+                    if (visited.add(route)) {
+                        for (int nextStop : routes[route]) {
+                            queue.offer(nextStop);
+                        }
+                    }
+                }
+            }
+            buses++;
+        }
+
+        return -1;
+    }
+}
