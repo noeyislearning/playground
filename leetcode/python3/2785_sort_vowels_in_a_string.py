63fe69b43d498b5a9c2deebe0ba042c69ef6b309
Noey Ignacio <hello@noeyislearning.dev>
2023-11-13 17:34:33 +0800

    build/leetcode: 2875 PY challenge, daily

---
+++ leetcode/python3/2785_sort_vowels_in_a_string.py
@@ -0,0 +1,25 @@
+"""
+ * 2785. Sort Vowels in a String
+ * Problem Link: https://leetcode.com/problems/sort-vowels-in-a-string/
+ * Solution Link (Personal):
+"""
+
+class Solution:
+  def sortVowels(self, s: str) -> str:
+    chars = list(s)
+    
+    vowels = []
+    for i, ch in enumerate(chars):
+      if ch in 'aeiouAEIOU':
+        vowels.append(ch)
+        chars[i] = ''
+        
+    vowels.sort()
+    
+    j = 0
+    for i, ch in enumerate(chars):
+      if ch == '':
+        chars[i] = vowels[j]
+        j += 1
+        
+    return ''.join(chars)
