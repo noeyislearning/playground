49184ee8c1817eb0fd86847d57bd557aa77e5eeb
Noey Ignacio <hello@noeyislearning.dev>
2023-11-13 18:09:49 +0800

    build/leetcode: 1174 SQL challenge

---
+++ leetcode/sql/mysql/1174_immediate_food_delivery_ii.sql
@@ -0,0 +1,19 @@
+-- 1174. Immediate Food Delivery II 
+-- Problem Link: https://leetcode.com/problems/immediate-food-delivery-ii/
+-- Solution Link (Personal):
+
+WITH CTE AS (
+  SELECT 
+    customer_id,
+    MIN(order_date) AS first_order_date,
+    CASE 
+      WHEN MIN(order_date) = MIN(customer_pref_delivery_date) THEN 1
+      ELSE 0 
+    END AS is_immediate
+  FROM Delivery 
+  GROUP BY customer_id
+)
+
+SELECT 
+  ROUND(AVG(is_immediate) * 100, 2) AS immediate_percentage
+FROM CTE;
