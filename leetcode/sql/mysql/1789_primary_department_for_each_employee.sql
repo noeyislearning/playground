c5a0cecb09298f410bebd8dfb9243e75ad92bdfb
Noey Ignacio <hello@noeyislearning.dev>
2023-11-14 12:23:19 +0800

    build/leetcode: 1789 SQL challenge

---
+++ leetcode/sql/mysql/1789_primary_department_for_each_employee.sql
@@ -0,0 +1,16 @@
+-- 1789. Primary Department for Each Employee
+-- Problem Link: https://leetcode.com/problems/primary-department-for-each-employee/
+-- Solution Link (Personal):
+
+SELECT employee_id, department_id
+FROM Employee
+WHERE primary_flag = 'Y'
+UNION
+SELECT employee_id, department_id
+FROM Employee
+WHERE employee_id IN (
+    SELECT employee_id
+    FROM Employee
+    GROUP BY employee_id
+    HAVING COUNT(*) = 1
+)
