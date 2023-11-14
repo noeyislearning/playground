4b188c8b027d8a4a4b429e53c4db6df2ce264047
Noey Ignacio <hello@noeyislearning.dev>
2023-11-14 23:47:47 +0800

    build: 2 bdt PY prepare

---
+++ hackerrank/python/basic-data-types/002_find_the_runner_up_score.py
@@ -0,0 +1,13 @@
+"""
+ * Find the Runner-Up Score!
+"""
+
+if __name__ == "__main__":
+    n = int(input())
+    arr = map(int, input().split())
+
+    us = list(set(arr))
+    us.sort(reverse=True)
+    rus = us[1]
+
+    print(rus)
