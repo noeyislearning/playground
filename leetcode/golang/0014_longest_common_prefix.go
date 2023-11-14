ebb6525c265178c1ccf1b853ab308aeb59b78298
Noey Ignacio <hello@noeyislearning.dev>
2023-11-14 22:44:54 +0800

    expt/leetcode: 14 Go challenge

---
+++ leetcode/golang/0014_longest_common_prefix.go
@@ -0,0 +1,23 @@
+/**
+ * 14. Longest Common Prefix
+ * Problem Link: https://leetcode.com/problems/longest-common-prefix/
+ * Solution Link (Personal):
+ */
+
+ func longestCommonPrefix(strs []string) string {
+	if len(strs) == 0 {
+			return ""
+	}
+	
+	prefix := strs[0]
+	for i := 1; i < len(strs); i++ {
+			for !strings.HasPrefix(strs[i], prefix) {
+					prefix = prefix[:len(prefix)-1]
+					if len(prefix) == 0 {
+							return ""
+					}
+			}
+	}
+	
+	return prefix
+}
