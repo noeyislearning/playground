253fe8cdf3a120a591acba6a1d3a11325824583d
Noey Ignacio <hello@noeyislearning.dev>
2023-11-14 12:23:10 +0800

    build/leetcode: 1731 SQL challenge

---
+++ leetcode/sql/mysql/1731_the_number_of_employees_which_report_to_each_employee.sql
@@ -0,0 +1,10 @@
+-- 1731. The Number of Employees Which Report to Each Employee
+-- Problem Link: https://leetcode.com/problems/the-number-of-employees-which-report-to-each-employee/
+-- Solution Link (Personal):
+
+SELECT m.employee_id, m.name, COUNT(r.employee_id) AS reports_count, ROUND(AVG(r.age)) AS average_age  
+FROM Employees m
+LEFT JOIN Employees r ON m.employee_id = r.reports_to
+WHERE r.employee_id IS NOT NULL
+GROUP BY m.employee_id
+ORDER BY m.employee_id;
