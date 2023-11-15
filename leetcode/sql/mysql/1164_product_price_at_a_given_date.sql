2cf14ad147844e76d39fc5315f932f047aacfef9
Noey Ignacio <hello@noeyislearning.dev>
2023-11-15 10:50:29 +0800

    build: 1164 sp SQL challenge

---
+++ leetcode/sql/mysql/1164_product_price_at_a_given_date.sql
@@ -0,0 +1,20 @@
+-- 1164. Product Price at a Given Date 
+-- Problem Link: https://leetcode.com/problems/product-price-at-a-given-date/
+-- Solution Link (Personal):
+
+SELECT p1.product_id, COALESCE(p2.new_price, 10) AS price
+FROM (
+    SELECT DISTINCT product_id
+    FROM Products
+) p1
+LEFT JOIN (
+    SELECT product_id, new_price
+    FROM (
+        SELECT *,
+               ROW_NUMBER() OVER(PARTITION BY product_id ORDER BY change_date DESC) AS rn
+        FROM Products
+        WHERE change_date <= '2019-08-16'
+    ) AS subquery
+    WHERE rn = 1
+) p2 ON p1.product_id = p2.product_id
+GROUP BY p1.product_id;
