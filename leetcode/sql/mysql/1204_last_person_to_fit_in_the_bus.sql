154298971e2f0093241e87d2f86a0951f6784f3f
Noey Ignacio <hello@noeyislearning.dev>
2023-11-15 10:59:31 +0800

    build: 1204 sp SQL challenge

---
+++ leetcode/sql/mysql/1204_last_person_to_fit_in_the_bus.sql
@@ -0,0 +1,13 @@
+-- 1204. Last Person to Fit in the Bus 
+-- Problem Link: https://leetcode.com/problems/last-person-to-fit-in-the-bus/
+-- Solution Link (Personal):
+
+SELECT person_name
+FROM (
+    SELECT q.*,
+           SUM(weight) OVER (ORDER BY turn) AS total_weight
+    FROM Queue q
+) AS subquery
+WHERE total_weight <= 1000
+ORDER BY turn DESC
+LIMIT 1;
