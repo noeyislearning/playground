b22adaab6413331ad2a8aad8a0c4379920d29fdd
Noey Ignacio <hello@noeyislearning.dev>
2023-11-15 10:15:16 +0800

    build: 1843 daily PY challenge

---
+++ leetcode/python3/1846_maximum_element_after_decreasig_and_rearranging.py
@@ -0,0 +1,17 @@
+"""
+ * 1846. Maximum Element After Decreasing and Rearranging
+ * Problem Link: https://leetcode.com/problems/maximum-element-after-decreasing-and-rearranging/
+ * Solution Link (Personal):
+"""
+
+from typing import List
+
+class Solution:
+    def maximumElementAfterDecrementingAndRearranging(self, arr: List[int]) -> int:
+        arr.sort() 
+        max_val = 1 
+
+        for i in range(1, len(arr)):
+            max_val = min(max_val + 1, arr[i])  
+            
+        return max_val
