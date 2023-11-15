4463dcf0bc1a6b9cc1ffb3ff5cdfa0a207f4269e
Noey Ignacio <hello@noeyislearning.dev>
2023-11-15 10:19:00 +0800

    build: 1843 daily Java challenge, expt

---
+++ leetcode/java/1846_maximum_element_after_decreasig_and_rearranging.java
@@ -0,0 +1,23 @@
+/**
+ * 1846. Maximum Element After Decreasing and Rearranging
+ * Problem Link: https://leetcode.com/problems/maximum-element-after-decreasing-and-rearranging/
+ * Solution Link (Personal):
+*/
+
+class Solution {
+    public int maximumElementAfterDecrementingAndRearranging(int[] arr) {
+        int n = arr.length;
+        int[] count = new int[n + 1];
+        
+        for (int num : arr) {
+            count[Math.min(num, n)]++;
+        }
+
+        int max = 0;
+        for (int i = 1; i <= n; i++) {
+            max = Math.min(max + count[i], i); 
+        }
+
+        return max;
+    }
+}
