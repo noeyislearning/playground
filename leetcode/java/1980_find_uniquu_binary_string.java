a71dea57a1c7bef810c45d2b7f32a07aab1e0cd6
Ibrahim Lincoln <jribrahimlincoln@gmail.com>
2023-11-16 09:24:40 +0800

    build: 1980 daily Java challenge
    Co-Authored-By: Noey Ignacio <hello@noeyislearning.dev>

---
+++ leetcode/java/1980_find_uniquu_binary_string.java
@@ -0,0 +1,32 @@
+/**
+ * 1980. Find Unique Binary String
+ * Problem Link: https://leetcode.com/problems/find-unique-binary-string/
+ * Solution Link (Personal):
+ */
+
+import java.util.HashSet;
+import java.util.Set;
+
+class Solution {
+    public String findDifferentBinaryString(String[] nums) {
+        int n = nums.length;
+        Set<String> set = new HashSet<>();
+        
+        for (String num : nums) {
+            set.add(num);
+        }
+        
+        for (int i = 0; i < (1 << n); i++) {
+            String binary = Integer.toBinaryString(i);
+            while (binary.length() < n) {
+                binary = "0" + binary;
+            }
+            
+            if (!set.contains(binary)) {
+                return binary;
+            }
+        }
+        
+        return "";
+    }
+}
