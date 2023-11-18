eea76cf34b566b2e9ecb6f3ef101681a85e07bea
Ibrahim Lincoln <jribrahimlincoln@gmail.com>
2023-11-18 21:35:51 +0800

    refactor: remove user's solution link
    Co-Authored-By: Noey Ignacio <hello@noeyislearning.dev>

--- 00_leetcode/golang/0001_two_sum.go
+++ 00_leetcode/golang/0001_two_sum.go
@@ -1,19 +1,18 @@
 /**
  * 1. Two Sum
  * Problem Link: https://leetcode.com/problems/two-sum/
- * Solution Link (Personal):
  */
 
 func twoSum(nums []int, target int) []int {
 	numMap := make(map[int]int)
-	
+
 	for i, num := range nums {
-			complement := target - num
-			if index, found := numMap[complement]; found {
-					return []int{index, i}
-			}
-			numMap[num] = i
+		complement := target - num
+		if index, found := numMap[complement]; found {
+			return []int{index, i}
+		}
+		numMap[num] = i
 	}
-	
+
 	return nil
 }
--- 00_leetcode/golang/0009_palindrome_number.go
+++ 00_leetcode/golang/0009_palindrome_number.go
@@ -1,19 +1,17 @@
 /**
  * 9. Palindrome Number
  * Problem Link: https://leetcode.com/problems/palindrome-number/
- * Solution Link (Personal):
  */
-
 
 func isPalindrome(x int) bool {
 	if x < 0 || (x%10 == 0 && x != 0) {
-			return false
+		return false
 	}
 
 	reversed := 0
 	for x > reversed {
-			reversed = reversed*10 + x%10
-			x /= 10
+		reversed = reversed*10 + x%10
+		x /= 10
 	}
 
 	return x == reversed || x == reversed/10
