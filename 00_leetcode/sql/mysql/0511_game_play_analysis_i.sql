12cb9a61337349c27035fc3142b3d15c7b5b7789
Ibrahim Lincoln <jribrahimlincoln@gmail.com>
2023-11-19 15:55:20 +0800

    refactor: remove personal solution links
    Co-Authored-By: Noey Ignacio <hello@noeyislearning.dev>

--- 00_leetcode/sql/mysql/0511_game_play_analysis_i.sql
+++ 00_leetcode/sql/mysql/0511_game_play_analysis_i.sql
@@ -1,6 +1,5 @@
 -- 511. Game Play Analysis I (Answer 1)
 -- Problem Link: https://leetcode.com/problems/game-play-analysis-i/
--- Solution Link (Personal):
 
 SELECT player_id, MIN(event_date) AS first_login
 FROM Activity
